eb7220d203a7b67f100c98c1f80aecb78514ec3b
andychen313 <andyc313@nycstudents.net>
2025-11-13 09:12:05 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import requests
-
 def getPoke(poke):
+    requests.get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
