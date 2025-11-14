80f0b22704235b3ccc03f088fc1bcf11f22b0211
andychen313 <andyc313@nycstudents.net>
2025-11-14 09:26:20 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 def getPoke(poke):
     requests.get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -15,4 +15,21 @@ def getPoke(poke):
     }
 
 pokemon = getPoke("Bulbasaur")
-print(pokemon['types'])
+print(pokemon['types']) """
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Pokemon Data")
+window.geometry("400x250")
+window.resizable(False, False)
+prompt=tk.Label(window, text = "Type your message below: ", font = ("Times New Roman", 14))
+prompt.pack(pady=10)
+entry = tk.Entry(window, font = ("Times New Roman", 14), width = 30)
+entry.pack(pady=5)
+result_label = tk.Label(window, text="", font=("Times New Roman", 14, "bold"), fg="blue")
+result_label.pack(pady=15)
+def pokemon_data():
+    text = entry.get()
+    
+window.mainloop()
