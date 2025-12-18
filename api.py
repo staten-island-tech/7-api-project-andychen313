7f47afe7b0b350a9050891e214773c1224574724
andychen313 <andyc313@nycstudents.net>
2025-12-18 09:26:04 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,25 +1,3 @@
-""" import requests
-def getPoke(poke):
-    requests.get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    
-    data = response.json()
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-
-pokemon = getPoke("Pikachu")
-print(pokemon['types'],['weight']) """
-
-import tkinter as tk
-import requests
-import threading
 
 """ def receive_api_data(fruits):
     try:
@@ -57,42 +35,10 @@ root.mainloop() """
 import requests
 import tkinter as tk
 
-""" root = tk.Tk()
-root.title("Fruit Data")
-root.geometry("900x600")
-result_label = tk.Label(root, text = "Fruit Found: ", font = ("Comic Sans", 14, "bold"), fg = "blue")
-result_label.pack(pady = 10)
-entry = tk.Entry(root, font = ("Comic Sans", 14), width = 10)
-entry.pack(pady=10)
-name_label = tk.Label(root, text = "", font = ("Comic Sans", 14), wraplength = 755, fg = "blue")
-id_label = tk.Label(root, text = "", font = ("Comic Sans", 14), wraplength = 755, fg = "blue")
-family_label = tk.Label(root, text = "", font = ("Comic Sans", 14), wraplength = 755, fg = "blue")
-nutritions_label = tk.Label(root, text = "", font = ("Comic Sans", 14), wraplength = 755, fg = "blue")
-name_label.pack(pady = 10)
-id_label.pack(pady = 10)
-family_label.pack(pady=10)
-nutritions_label.pack(pady=10)
-
-def receive_api_data():
-    try:
-        fruits = entry.get()
-        response = requests.get(f"https://www.fruityvice.com/api/fruit/{fruits.lower()}")
-        
-        data = response.json()
-        name_label.config(text = f"Name: {data["name"]}")
-        id_label.config(text = f"Id: {data["id"]}")
-        family_label.config(text = f"Family: {data["family"]}")
-        nutritions_label.config(text = f"Nutritions: {data["nutritions"]}")
-    except Exception as e:
-        print(f"Error: {e}")
-
-button = tk.Button(root, text = "Press to fetch fruit data!", font = ("Comic Sans", 14), bg = "black", fg = "white", command = receive_api_data)
-button.pack(pady=40)
-root.mainloop() """
-
 root = tk.Tk()
 root.title("Fruit Data")
 root.geometry("900x600")
+root.resizable(False, False)
 result_label = tk.Label(root, text = "Fruit Found: ", font = ("Comic Sans", 14, "bold"), fg = "blue")
 result_label.pack(pady = 10)
 entry = tk.Entry(root, font = ("Comic Sans", 14), width = 10)
@@ -116,11 +62,11 @@ def receive_api_data():
         id_label.config(text = f"Id: {data["id"]}")
         family_label.config(text = f"Family: {data["family"]}")
         nutritions_label.config(text = f"Nutritions: {data["nutritions"]}")
-        update_label(data)
-    except Exception as e:
-        update_label("Error: Not Found")
-def update_label(text):
-    name_label.config(text=text)
+    except Exception:
+        name_label.config(text= "Error! Fruit not found!")
+        id_label.config(text = "")
+        family_label.config(text = "")
+        nutritions_label.config(text = "")
 
 button = tk.Button(root, text = "Press to fetch fruit data!", font = ("Comic Sans", 14), bg = "black", fg = "white", command = receive_api_data)
 button.pack(pady=40)
